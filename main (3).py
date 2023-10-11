611ac096dc47b97c7f52a7fb1e2d37e7f1252230
Sarojinimucs138 <144533140+Sarojinimucs138@users.noreply.github.com>
2023-10-11 13:42:39 +0530

    Add files via upload

--- main (3).py
+++ main (3).py
@@ -1,34 +1,10 @@
-class BankAccount:
-  def __init__(self, 
-account_number, account_holder_name, initial_balance=0.0):
-    self.__account_number = account_number
-    self.__account_holder_name = account_holder_name
-    self.__account_balance = initial_balance
-  def deposit(self,amount):
-    if amount > 0:
-      self.__account_balance += amount
-      #self.__account_balance = self.__account_balance+amount
-      print("Deposited ₹{}. New balance: ₹{}".format 
-(amount,self. __account_balance))
-    else:
-      print("Invalid deposit amount.")
-      
-  def withdraw(self, amount):
-    if amount > 0 and amount <= self.__account_balance:
-      self. __account_balance -= amount
-      print (" withdraw ₹{}. New balance: ₹{}".format (amount, self. __account_balance))
-    else:
-      print("Invalid withdrawal amount or insufficient balance.")
-  def display_balance(self):
-    print(" Account balance for {} (Account #{}): {}".format (self.__account_holder_name,  self.__account_number, self. __account_balance ))
-
-
-#create an instance of the BankAccount class 
-account = BankAccount(account_number="123 456789",account_holder_name=" Hari brabu", initial_balance=5000.0) 
-
-    #Test deposit and withdrawal functionality
-account. display_balance () 
-account. deposit (500.0) 
-account. withdraw(200.0)
-account. withdraw (20000.0)
-account. display_balance() 
+def linearSearchProduct(productList,targetProduct):
+  indices = []
+  for index,product in enumerate(productList):
+    if product == targetProduct:
+      indices.append(index)
+  return indices
+products = ["keyboard", "printer", "mouse", "keyboard", "touchpad", "keyboard"]
+target = "keyboard"
+result = linearSearchProduct(products,target)
+print(result)
